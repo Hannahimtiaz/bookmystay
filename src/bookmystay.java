d899a55188a4eeb3e4c4ba3b29dbb982d5f5e04f
Hannahimtiaz <hannahkhan680@gmail.com>
2026-03-13 14:13:35 +0530

    UC1: Add welcome message module

--- src/bookmystay.java
+++ src/bookmystay.java
@@ -1,2 +1,10 @@
 public class bookmystay {
+
+    public static void main(String[] args) {
+        System.out.println("=======================================");
+        System.out.println("   Welcome to Hotel Booking System");
+        System.out.println("   Application started successfully");
+        System.out.println("=======================================");
+    }
+
 }
